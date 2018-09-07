3b1b1e7e9c2812dec509dc12c60150c318057e8d
Alli Tzadok <allisontzadok@gmail.com>
2018-09-07 15:34:54 -0500

    add search bar and app function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyBwUeFLKCYeh0yqA4jVZmT959OfUaInOwk'
 
-import App from './components/app';
-import reducers from './reducers';
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create a new component and this component should produce some HTML
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this components generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
